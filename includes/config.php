5436e7727e1a4a122e4c0c95e669c853b43afbd1
Guilherme Rauzes <guirauzes@gmail.com>
2025-02-25 01:21:26 -0300

    Update config.php
    Adicionado banco de dados no servidor em nuvem para pessoas externas conseguirem logar e acessar pelo app do Vercel

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost';
-$dbname = 'ProjetoFacul';
-$username = 'root';
-$password = 'Admin123';
+$host = 'sql.freedb.tech';
+$dbname = 'freedb_projetofacul';
+$username = 'freedb_projeto_root';
+$password = '?R2b!e$pDfkqXBX';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
